902135b7eca6a3cdd63e058af670831758578128
Noman Ahmad <noman.ahmad456@gmail.com>
2022-04-17 23:45:04 +0530

    database change/env variable set

--- Gemfile
+++ Gemfile
@@ -12,8 +12,8 @@ gem 'bcrypt', '~> 3.1.7'
 gem 'devise'
 
 # Use sqlite3 as the database for Active Record
-gem "sqlite3", "~> 1.4"
-
+# gem "sqlite3", "~> 1.4"
+gem "pg"
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", "~> 5.0"
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,12 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_04_08_135611) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "post_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
